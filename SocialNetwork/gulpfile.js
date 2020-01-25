6178509afbea5c7162dbd3ae7e00a3e1f50114d1
Matt Flynn <matt12384@live.co.uk>
2020-01-25 21:24:57 +0000

    Added tailwindcss

---
+++ SocialNetwork/gulpfile.js
@@ -0,0 +1,39 @@
+﻿var gulp = require('gulp'),
+    plumber = require('gulp-plumber');
+
+gulp.task('css', function () {
+    var postcss = require('gulp-postcss'),
+        tailwindcss = require('tailwindcss'),
+        sass = require('gulp-sass');
+
+    return gulp.src('./Assets/scss/site.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([
+            tailwindcss('tailwind.config.js'),
+            require('autoprefixer')
+        ]))
+        .pipe(gulp.dest('wwwroot/css/'));
+});
+
+gulp.task('watch', gulp.series('css', function () {
+    gulp.watch([
+        './tailwind.config.js',
+        './Pages/**/*.cshtml',
+        './Pages/**/*.razor',
+        './Shared/**/*.razor',
+        './Assets/scss/**/*/scss'
+    ],
+    gulp.series('css'));
+}));
+
+gulp.task('default', gulp.series('css', function () {
+    var csso = require('gulp-csso'),
+        rename = require('gulp-rename');
+
+    return gulp.src('./wwwroot/css/site.css')
+        .pipe(plumber())
+        .pipe(csso())
+        .pipe(rename('site.min.css'))
+        .pipe(gulp.dest('./wwwroot/css'));
+}));
